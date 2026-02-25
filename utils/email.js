ce354ed2c919d587e4719647a3041f03c119c8c2
bet <lolalbert777@gmail.com>
2026-02-25 22:17:06 +0700

    fixing pug path

--- utils/email.js
+++ utils/email.js
@@ -37,7 +37,7 @@ class Email {
   }
 
   async send(template, subject) {
-    const templatePath = path.join(process.cwd(), 'views', 'passwordreset.pug');
+    const templatePath = path.join(process.cwd(), 'views', `${template}.pug`);
 
     // const html = pug.renderFile(`${__dirname}/../views/${template}.pug`, {
     const html = pug.renderFile(templatePath, {
